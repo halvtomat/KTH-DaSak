def5a74b9fa74b805f1f9773ab6f55d76ef9968a
Daniel <danne_1998@hotmail.se>
2021-02-15 14:23:38 +0100

    side_channel exercise done

--- Exercises/side_channels/sec_crypto.py
+++ Exercises/side_channels/sec_crypto.py
@@ -5,6 +5,10 @@ class SecCrypto():
         for a in range(len(T)):
             cache.write_address(T_address+a, T[a])
     
+    def clear_cache(self, cache):
+        for i in range(len(T)):
+            cache.write_address(T_address+i, '0'*32)
+    
     def feistel_encrypt(self, cache, msg, key):
         l0 = ord(msg[0])
         r0 = ord(msg[1])
@@ -14,4 +18,6 @@ class SecCrypto():
         r2 = cache.read_address(r1)
         r2 = l1 ^ ord(key[1]) ^r2[0]
         l2 = l1
+        self.clear_cache(cache)
         return chr(l2)+chr(r2)
+
